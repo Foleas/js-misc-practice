19050751013e5ce4b7b17644fa55c0f79fab0c84
Foleas <sebastianfilipetto@gmail.com>
2022-11-24 15:52:16 -0300

    uniqueSorted updated

--- uniqueSorted/index.js
+++ uniqueSorted/index.js
@@ -1,17 +1,21 @@
 const nums = [1, 27, 28, 3, 2, 3, 3, 2, 8, 1, 9, 0, 3];
 
 const uniqueSorted = (arr) => {
-  return [...new Set(arr)].sort((a, b) => a - b);
+  return [...new Set(arr)].sort();
 };
 
 const uniqueSorted2 = (arr) => {
-  return arr.filter((value, index, array) => {
-    // console.log("value", value);
-    // console.log("index", index);
-    // console.log("array", array);
-    // console.log("indexOf", array.indexOf(value));
-    return array.indexOf(value) === index;
-  });
+  return arr
+    .filter((value, index, array) => {
+      // console.log("value", value);
+      // console.log("index", index);
+      // console.log("array", array);
+      // console.log("indexOf", array.indexOf(value));
+      return array.indexOf(value) === index;
+    })
+    .sort();
+  // .sort((a, b) => a - b)
+  // .sort((a, b) => b - a)
 };
 
 console.log(uniqueSorted(nums)); // [0,1,2,3,8,9,27,28]
